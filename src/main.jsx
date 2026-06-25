e25269183b2f3a17b203a37c34a0483cf2c7ba9d
zrewt <bennyrosenberg0628@outlook.com>
2026-06-25 08:18:33 -0400

    describe what you changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { inject } from '@vercel/analytics'
 import './index.css'
+import App from './App.jsx'
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
