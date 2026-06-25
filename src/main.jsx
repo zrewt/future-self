5f8b4c66912b2b05187a5b175fc564e387891645
zrewt <bennyrosenberg0628@outlook.com>
2026-06-25 08:22:04 -0400

    describe what you changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { inject } from '@vercel/analytics'
-import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.jsx'
 
